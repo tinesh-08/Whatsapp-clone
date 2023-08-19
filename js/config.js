a5e6b03fc1acbe17e904e9733597a17bdf5a97c3
Tinesh Raj T A <tine20229.it@rmkec.ac.in>
2023-08-19 11:45:00 +0530

    Initial commit

---
+++ js/config.js
@@ -0,0 +1,15 @@
+const config = {
+    apiKey: "AIzaSyAYRl8zQbmRoRVP6GK2Y3pG9_OCNfok-GM",
+    authDomain: "learn-kid-ba3eb.firebaseapp.com",
+    databaseURL: "https://learn-kid-ba3eb-default-rtdb.firebaseio.com",
+    projectId: "learn-kid-ba3eb",
+    storageBucket: "learn-kid-ba3eb.appspot.com",
+    messagingSenderId: "1078661122231",
+    appId: "1:1078661122231:web:17996606e28ba315572532",
+    measurementId: "G-YRQY8T6D6F",
+    CometChatAppId: "226386c7301eaa71",
+    CometChatRegion: "us",
+    CometChatAuthKey: "589bd8bd8a599fee28e0c78148b0119d1ae6c0fc",
+    CometChatAPIKey: "4cb54e8956788afc4fb51ddbd15ed52f7addaa6d",
+    CometChatWidgetId: "c65f706c-fe02-41c9-a0dd-01a351a209c8"
+  };
